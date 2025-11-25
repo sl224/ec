1922ac2db067d4ea487386a682b08f6f69a3da29
shan <shan.liyanage@ngc.com>
2025-11-25 12:50:11 -0800

    update

--- src/e2ude_core/services/fs_scanner.py
+++ src/e2ude_core/services/fs_scanner.py
@@ -4,20 +4,20 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 import os
 from tqdm import tqdm
+from collections import deque
 
 logger = logging.getLogger(__name__)
 
 
-def _search_dir(search_path: Path, zips_found, top):
+def _search_dir(search_path):
     add_dirs = []
+    zips_found = []
     for res in os.scandir(search_path):
         if res.is_dir(follow_symlinks=False):
             add_dirs.append(res.path)
         elif res.is_file and res.name.endswith(".e2d.zip"):
             zips_found.append(Path(res.path))
-            if len(zips_found) == top:
-                break
-    return add_dirs
+    return add_dirs, zips_found
 
 
 def scan_for_rsm_zips(search_path: Path, top=None):
@@ -25,30 +25,28 @@ def scan_for_rsm_zips(search_path: Path, top=None):
         raise ValueError(f"Search path does not exist: {search_path}")
 
     if top is None:
-        top = float("inf")
+        top = float('inf')
 
     logger.info(f"Scanning {search_path} for RSM zips...")
-    search_dirs = [search_path]
     zips_found = []
-    futures = []
     pbar = tqdm()
     with ThreadPoolExecutor() as executor:
-        while search_dirs and len(zips_found) < top:
-            while search_dirs:
-                future = executor.submit(
-                    _search_dir, search_dirs.pop(), zips_found, top
-                )
-                futures.append(future)
-            while len(zips_found) < top and futures:
-                prior = len(zips_found)
-                search_dirs.extend(futures.pop().result())
-                new_zips_found_count = len(zips_found) - prior + 1
-                pbar.update(new_zips_found_count)
+        futures_running = deque([executor.submit(_search_dir, search_path)])
+        while futures_running:
+            f = futures_running.popleft()
+            if f.done():
+                dirs_found, zips_found_sub = f.result()
+                futures_running.extend(executor.submit(_search_dir, d) for d in dirs_found)
+                zips_found.extend(zips_found_sub)
+                pbar.update(len(zips_found_sub))
+            else:
+                print('Still running')
+                futures_running.append(f)
     pbar.close()
     logger.info(f"Found {len(zips_found)} zips.")
     return zips_found
 
 
 if __name__ == "__main__":
-    test = Path(r"\\esidme24\#ESIDME24\PUBLIC\E2 Stuff\ALE RSM Data Archive\166502")
+    test = Path(r"\\esidme24\#ESIDME24\PUBLIC\E2 Stuff\ALE RSM Data Archive")
     zips_found = scan_for_rsm_zips(test)
