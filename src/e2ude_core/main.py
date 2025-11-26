771d057770704ac0b97779305e1851da507483c6
shan <shan.liyanage@ngc.com>
2025-11-25 22:00:20 -0800

    test

--- src/e2ude_core/main.py
+++ src/e2ude_core/main.py
@@ -6,12 +6,8 @@ from e2ude_core.config import settings
 from e2ude_core.db import access as sql_io
 from e2ude_core.db.setup import initialize_database, register_folders_bulk
 from e2ude_core.logging_conf import setup_logging
-<<<<<<< HEAD
 from e2ude_core.orchestration.pipeline import StagingPipeline
 from e2ude_core.services.discovery import discover_network_zips
-=======
-from e2ude_core.services.fs_scanner import scan_for_rsm_zips
->>>>>>> refs/remotes/origin/fix_folder
 
 logger = logging.getLogger(__name__)
 
@@ -37,8 +33,8 @@ def main():
         initialize_database(main_eng, reset_tables=True)
 
         # 1. Discovery (Fast Walk)
-        # scan_root = Path(r"\\esidme24\#ESIDME24\PUBLIC\E2 Stuff\ALE RSM Data Archive")
-        scan_root = Path(r"tests/static_assets/")
+        scan_root = Path(r"\\esidme24\#ESIDME24\PUBLIC\E2 Stuff\ALE RSM Data Archive")
+        # scan_root = Path(r"tests/static_assets/")
 
         valid_paths = discover_network_zips(scan_root, max_workers=1024)
         if not valid_paths:
@@ -54,8 +50,8 @@ def main():
             zip_paths=valid_paths,
             folder_id_map=folder_id_map,
             staging_root=STAGING_ROOT,
-            num_consumers=8,
-            queue_size=16,
+            num_consumers=32,
+            queue_size=64
         )
         pipeline.run()
 
